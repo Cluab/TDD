b700f27809a406d9ac49f8d25a0ab5ef8777668d
ginabeki <ginabeki3@gmail.com>
2023-02-09 16:33:43 +0300

    Add logic to fizzbuzz, reverse, and factorial method

--- solver.rb
+++ solver.rb
@@ -1,13 +1,27 @@
 class Solver
-  def factorial(int)
-    int
+  def factorial(num)
+    if [0, 1].include?(num)
+      1
+    elsif num.negative?
+      raise 'Invalid argument: only positive integers and 0 are allowed'
+    else
+      num * factorial(num - 1)
+    end
   end
 
   def reverse(str)
-    str
+    str.reverse
   end
 
-  def fizzbuzz(int)
-    int
+  def fizzbuzz(num)
+    if (num % 3).zero? && (num % 5).zero?
+      'fizzbuzz'
+    elsif (num % 3).zero?
+      'fizz'
+    elsif (num % 5).zero?
+      'buzz'
+    else
+      num.to_s
+    end
   end
 end
