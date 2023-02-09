3ac27c1eabacc8bb6d474f7103665269aed4640d
Ibrahim Suhail Al-Yousefi <cluab11222@gmail.com>
2023-02-09 14:34:37 +0200

    add factorial method tests

---
+++ spec/solver_spec.rb
@@ -0,0 +1,21 @@
+describe Solver do
+  let(:solver) { Solver.new }
+
+  describe '#factorial' do
+    context 'with positive integers' do
+      it 'returns the factorial for that number' do
+        expect(solver.factorial(5)).to eq(120)
+      end
+
+      it 'returns the factorial of 0 as 1' do
+        expect(solver.factorial(0)).to eq(1)
+      end
+    end
+
+    context 'with negative integers' do
+      it 'raises an exception' do
+        expect { solver.factorial(-1) }.to raise_error('Invalid argument: only positive integers and 0 are allowed')
+      end
+    end
+  end
+end
